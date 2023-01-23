a439ad86a2a8203e8e2fdc52c7a2eb5414fae036
Alex Serrano Marull <alex.serrano01@estudiant.upf.edu>
2023-01-23 19:33:58 +0100

    first part done
    first conection between client and server

--- client.py
+++ client.py
@@ -9,33 +9,33 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connecting to the server
 client.connect((SERVER_IP, PORT))
+aux = ""
 
 while True:
-    print("This is a calculator that is able to perform addition, subtraction, multiplication and division")
+    print("This is a simple calculator that is able to perform addition, subtraction, multiplication and division")
 
-    # Get the input from the user
-    first_value = input("\nEnter the first value of the operation: ")
-    operation = input("\nEnter the operation you want to perform: ")
-    second_value = input("\nEnter the second value: ")
-
-    # If user wants to terminate the connection with the server socket they can type y
-    aux = input("\nDo you want to terminate? [y/n]")
     if aux == "y":
+        client.send(aux.encode())
         break
 
-    # Group the inputs in a list to send to the server
-    sentence = [first_value, operation, second_value]
+    # Get the input from the user
+    first_value = str(input("\nEnter the first value of the operation: "))
+    operation = str(input("\nEnter the operation you want to perform (+, -, *, /): "))
+    second_value = str(input("\nEnter the second value: "))
+
+    # Group the inputs and send them to the server
+    msg = first_value + " " + operation + " " + second_value
 
     # Here we send the user input to the server socket
-    client.send(sentence.encode())
+    client.send(msg.encode())
 
     # Here we receive the output from the server socket
     answer =  client.recv(1024)
     print("\nThe answer is ", answer.decode())
+    print("\n(Note that if the answer is 0, something might have gone wrong! Check that the input was correct)")
 
-    # If user wants to terminate the connection with the server socket they can type y
+    # If user wants to terminate the connection with the server socket they can type 'y'
     aux = input("\nDo you want to terminate? [y/n]")
-
 
 client.close()
 
--- server.py
+++ server.py
@@ -12,4 +12,41 @@ print("\nWaiting for request...")
 
 # Now everything is ready for a user input
 clientConnection, clientAddress = server.accept()
+print("Connected client: ", clientAddress)
+msg = ""
+
+# Run infinite loop
+while True:
+    data = clientConnection.recv(1024)
+    msg = data.decode()
+    if msg == "y":
+        print("Connection is over")
+        break
+
+    print("Equation received")
+    result = 0
+    str_list = msg.split()
+    first = str_list[0]
+    operation = str_list[1]
+    second = str_list[2]
+
+    # Turn the number to integers
+    num1 = int(first)
+    num2 = int(second)
+
+    # We can now perform the operations for the calculator
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        result = num1 / num2
+
+    print("Sending result to client...")
+    output = str(result)
+    clientConnection.send(output.encode())
+clientConnection.close()
+
 
