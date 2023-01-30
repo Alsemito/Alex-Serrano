1fcb4ccaa15a2f7f89e36443477a0a8999472e83
Alex Serrano Marull <alex.serrano01@estudiant.upf.edu>
2023-01-30 17:15:51 +0100

    CLI client v1

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((LOCALHOST, PORT))
 server.listen(1)
 print("Server started")
-print("\nWaiting for request...")
+print("Waiting for request...")
 
 # Now everything is ready for a user input
 clientConnection, clientAddress = server.accept()
